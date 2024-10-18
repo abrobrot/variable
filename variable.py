07645bfeb90a5366b3431aa56d730eb92bd9c904
abrobrot <olegstakhov210@gmail.com>
2024-10-18 20:02:11 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,5 @@
+number_of_dz = 12
+hour = 1.5
+language = 'Python'
+time = hour / number_of_dz
+print ('Курс:', language,',  всего задач:',number_of_dz,',  затрачено часов:',hour,',  среднее время выполнения:',time,'часа')
